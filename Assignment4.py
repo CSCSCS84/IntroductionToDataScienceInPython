eaff1f8458203e325a72ec22447e3ad0be109826
CSCSCS84 <christoph.stettin@gmx.net>
2018-06-12 13:43:33 +0200

    Solution Assignment4

--- Assignment4.py
+++ Assignment4.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 
 def get_list_of_university_towns():
@@ -100,6 +101,7 @@ def convert_housing_data_to_quarters():
         homes[q] = homes[vcopy].mean(axis=1)
     columnsState.extend(quarters)
     homes = homes[columnsState]
+    homes['State']=homes['State'].map( {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', 'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan', 'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico', 'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana', 'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California', 'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island', 'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'})
     homes.set_index(['State', 'RegionName'], inplace=True)
     return homes
 
@@ -122,9 +124,20 @@ def run_ttest():
 
     recessionStart=get_recession_start()
     recessionBottom=get_recession_bottom()
+    housePrices=convert_housing_data_to_quarters()
+    housePricesRecession=housePrices[recessionStart]-housePrices[recessionBottom]
 
-    return "ANSWER"
+
+    univerityTowns=get_list_of_university_towns()
+    print(housePricesRecession)
+    universityTownsCopy=univerityTowns.set_index(['State', 'RegionName'])
+    a=housePricesRecession.loc[universityTownsCopy.index]
+    b=housePricesRecession.loc[housePricesRecession.index.difference(universityTownsCopy.index)]
+    a=a.dropna()
+
+    test=stats.ttest_ind(a, b,equal_var = False)
+
+    return (True,test.pvalue,'university town')
 
 # convert_housing_data_to_quarters()
-#unis=get_list_of_university_towns()
-print(convert_housing_data_to_quarters())
+print(run_ttest())
